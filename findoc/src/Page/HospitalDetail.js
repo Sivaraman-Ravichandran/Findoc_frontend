ae5495adb89cffc359f6cbcb9aa827c2b3d5fc7d
Sivaraman-Ravichandran <sivcloud.12@gmail.com>
2024-07-31 20:46:33 +0530

    again modified the HospitalDetail

--- findoc/src/Page/HospitalDetail.js
+++ findoc/src/Page/HospitalDetail.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import DoctorCard from "../Component/DoctorCard";
 import "./HospitalDetail.css";
 import ContactForm from "../Component/ContactForm";
+import NavBar from "./NavBar";
 const hospitalsData = [
   {
     id: 1,
@@ -190,92 +191,95 @@ const HospitalDetail = () => {
   );
 
   return (
-    <div className="hospital-detail-container">
-      <div className="hospital-about">
-        <img src={hospital.image} alt="error" className="hospital-image" />
-        <div className="hosp-head">
-          <div className="hosp-detail">
-            <h1>{hospital.name}</h1>
-            <div className="location">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="25"
-                height="25"
-                fill="white"
-                class="bi bi-geo-alt"
-                viewBox="0 0 16 16"
-              >
-                <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
-                <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
-              </svg>
-              <p>{hospital.address}</p>
+    <>
+      <NavBar />
+      <div className="hospital-detail-container">
+        <div className="hospital-about">
+          <img src={hospital.image} alt="error" className="hospital-image" />
+          <div className="hosp-head">
+            <div className="hosp-detail">
+              <h1>{hospital.name}</h1>
+              <div className="location">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25"
+                  height="25"
+                  fill="white"
+                  class="bi bi-geo-alt"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
+                  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                </svg>
+                <p>{hospital.address}</p>
+              </div>
+            </div>
+            <div className="contact-form">
+              <ContactForm name={hospital.name} />
             </div>
           </div>
-          <div className="contact-form">
-            <ContactForm name={hospital.name} />
+        </div>
+        <div className="phone">
+          <h1>{hospital.name}</h1>
+          <a class="phone-link" href="tel:+916383262389">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="50"
+              fill="currentColor"
+              class="bi bi-telephone-fill"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"
+              />
+            </svg>
+            <p>+917969126494</p>
+          </a>
+        </div>
+        <div className="about">
+          <h1>About</h1>
+          <p>{hospital.about}</p>
+        </div>
+        <div>
+          <h1 style={{ textAlign: "center" }}>Doctors</h1>
+
+          <div className="doctor-cards-container">
+            {filteredDoctors.map((doctor, index) => (
+              <DoctorCard key={index} {...doctor} />
+            ))}
           </div>
         </div>
-      </div>
-      <div className="phone">
-        <h1>{hospital.name}</h1>
-        <a class="phone-link" href="tel:+916383262389">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="50"
-            height="50"
-            fill="currentColor"
-            class="bi bi-telephone-fill"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"
-            />
-          </svg>
-          <p>+917969126494</p>
-        </a>
-      </div>
-      <div className="about">
-        <h1>About</h1>
-        <p>{hospital.about}</p>
-      </div>
-      <div>
-        <h1 style={{ textAlign: "center" }}>Doctors</h1>
-
-        <div className="doctor-cards-container">
-          {filteredDoctors.map((doctor, index) => (
-            <DoctorCard key={index} {...doctor} />
-          ))}
+        <div className="services">
+          <h2>Services</h2>
+          <ul>
+            {hospital.services.map((service, index) => (
+              <li key={index}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  className="bi bi-check-circle-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                </svg>
+                {service}
+              </li>
+            ))}
+          </ul>
         </div>
-      </div>
-      <div className="services">
-        <h2>Services</h2>
-        <ul>
-          {hospital.services.map((service, index) => (
-            <li key={index}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill="currentColor"
-                className="bi bi-check-circle-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
-              </svg>
-              {service}
-            </li>
-          ))}
-        </ul>
-      </div>
 
-      <h2>News</h2>
-      <p>{hospital.news}</p>
-      <h2>Events</h2>
-      <p>{hospital.events}</p>
-      <h2>Contact Information</h2>
-      <p>{hospital.contact}</p>
-    </div>
+        <h2>News</h2>
+        <p>{hospital.news}</p>
+        <h2>Events</h2>
+        <p>{hospital.events}</p>
+        <h2>Contact Information</h2>
+        <p>{hospital.contact}</p>
+      </div>
+    </>
   );
 };
 
